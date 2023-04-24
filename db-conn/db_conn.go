4cf11843278e35a2c79e8b5e3fa7f5e26e7a9323
iivkis <razmolodinivan@mail.ru>
2023-04-24 14:30:55 +0700

    +

---
+++ db-conn/db_conn.go
@@ -0,0 +1,17 @@
+package dbconn
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/micro-it-freelance/core/config"
+)
+
+func NewDBConn() *sqlx.DB {
+	db, err := sqlx.Connect("pgx", fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable",
+		config.DB.Name, config.DB.User, config.DB.Password, config.DB.Host, config.DB.Port))
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
---
+++ grpc/listener.go
@@ -0,0 +1,16 @@
+package grpc
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/micro-it-freelance/core/config"
+)
+
+func NewGRPCListener() net.Listener {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", config.GRPC.Port))
+	if err != nil {
+		panic(err)
+	}
+	return listener
+}
