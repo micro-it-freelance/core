76c1eaa03e17ae74cec34d201a2c28fe172b5f7b
iivkis <razmolodinivan@mail.ru>
2023-04-24 20:44:40 +0700

    +

---
+++ database/connection.go
@@ -0,0 +1,22 @@
+package dbconn
+
+import (
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/micro-it-freelance/core/config"
+)
+
+func NewDBConnection() *sqlx.DB {
+	source := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable", 
+	config.DB.Name, config.DB.User, config.DB.Password, config.DB.Host, config.DB.Port)
+
+	db, err := sqlx.Connect("pgx", source)
+	if err != nil {
+		panic(err)
+	}
+	
+	return db
+}
